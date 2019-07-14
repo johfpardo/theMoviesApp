33886416473a475761f341d850769345cce24b2d
Jhon Fredy Pardo Patiño <jfpardopat@gmail.com>
2019-07-14 14:22:29 -0500

    Add movie detail view

---
+++ TheMoviesApp/TheMoviesApp/Screens/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MovieDetailViewController.swift
+//  TheMoviesApp
+//
+//  Created by Jhon Fredy Pardo Patiño on 7/14/19.
+//  Copyright © 2019 Jhon Fredy Pardo Patiño. All rights reserved.
+//
+
+import UIKit
+import PINRemoteImage
+
+class MovieDetailViewController: UIViewController {
+
+    var movie : Movie!
+    
+    @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var overviewText: UILabel!
+    @IBOutlet weak var releaseDateText: UILabel!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(movie ?? "")
+        navigationItem.title = movie.title
+        if let url = URL(string: ConstantsRestApi.urlAccessPosterDetail+movie.backdrop_path) {
+            movieImg.pin_updateWithProgress = true
+            movieImg.pin_setImage(from: url)
+        }
+        overviewText.text = movie.overview
+        releaseDateText.text = movie.release_date
+    }
+
+}
